282ea64f8a1d76ec6ed942b3a41ee1bfbadbbf03
Patryk Napiórkowski <patryk.napiorkowski95@gmail.com>
2025-07-04 13:18:22 +0200

    inital commit

---
+++ library.py
@@ -0,0 +1,8 @@
+import json
+
+# with open('books.json', 'r', encoding="utf-8") as file:
+#     data = json.load(file)
+
+# print(data[0])
+
+print('Witaj w naszej szkolnej bibliotece! Co chciałbyś zrobić?\n')
