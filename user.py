18de160d9a7497e3655f293f12228cdc2a17eafd
Patryk Napiórkowski <patryk.napiorkowski95@gmail.com>
2025-07-21 11:10:17 +0200

    changed name UsersBase to UsersManager and prepered names of possible methods in this class

--- user.py
+++ user.py
@@ -1,13 +1,52 @@
 import uuid
 
+#Może wykorzystać base64 dla password?
 class User:
-    def __init__(self, user_id, user_name, user_surname, borrowed_books):
+    def __init__(self, user_id, user_name, user_surname, password, borrowed_books):
         self.id = user_id or str(uuid.uuid4())
         self.name = user_name
         self.surname = user_surname
+        self.password = password
         self.borrowed_books = borrowed_books or []
         
-        
-class UserBase:
-    def __init__(self):
+    def borrow_book():
         pass
+    
+    def return_book():
+        pass
+
+class UsersManager:
+    def __init__(self, file_path = 'storage/users.json'):
+        self.file_path = file_path
+        self.users_list = self.load_users()
+
+    def load_users(self):
+        print("DUPA")
+        
+    def save_users(self):
+        print("zapisano")
+    def check_password(self): #Czy spełnia wymogi
+        print("sprawdzamy hasło")
+
+    def create_user(self):
+        print("Dodano użytkownia")
+        
+    def delete_user(self):
+        print("Usunięto użytkownika")
+        
+    def find_user(self):
+        print("Znaleziono użytkownika")
+        
+    def edit_user(self):
+        print("Zmieniono dane")
+        
+    def log_user(self):
+        pass
+    
+    def logout_user():
+        pass
+    
+    def display_all_users():
+        pass
+        
+    
